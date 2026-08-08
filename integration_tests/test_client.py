8089c7db1e98c1a1fb27a77e1688a726b5f5ba23
Noah Groß <me@codesalat.dev>
2026-08-08 12:40:17 +0200

    test: rename assertion due to new product name

--- integration_tests/test_client.py
+++ integration_tests/test_client.py
@@ -59,7 +59,7 @@ def test_get_article():
     response = picnic.get_article("s1018620")
     assert isinstance(response, Article)
     assert response.id == "s1018620"
-    assert response.name == "Gut&Günstig H-Milch 3,5%"
+    assert response.name == "Gut&Günstig H-Vollmilch 3,5%"
 
 
 def test_get_article_with_category_name():
@@ -72,7 +72,7 @@ def test_get_article_with_category_name():
 def test_get_article_by_gtin():
     response = picnic.get_article_by_gtin("4311501044209")
     assert response.id == "s1018620"
-    assert response.name == "Gut&Günstig H-Milch 3,5%"
+    assert response.name == "Gut&Günstig H-Vollmilch 3,5%"
 
 
 def test_get_article_by_gtin_unknown():
